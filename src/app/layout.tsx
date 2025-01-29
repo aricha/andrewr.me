81060e32ee8efc627dd88c50d439a75087514ceb
Andrew Richardson <andrewr114@gmail.com>
2025-01-29 11:31:08 -0800

    Add favicon and Open Graph support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,31 @@
 import { Inter } from 'next/font/google'
 import { Providers } from './providers'
 import './globals.css'
+import { getCldOgImageUrl } from 'next-cloudinary'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Andrew Richardson',
   description: 'Portfolio of Andrew Richardson, a software engineer and traveler.',
+  icons: {
+    icon: { url: '/icon.svg', type: 'image/svg+xml' },
+  },
+  openGraph: {
+    title: 'Andrew Richardson',
+    description: 'Portfolio of Andrew Richardson, a software engineer and traveler.',
+    images: [
+      {
+        url: getCldOgImageUrl({
+          src: 'home/profile',
+          width: 1200,
+          height: 630,
+          crop: 'fill',
+        }),
+        alt: 'Andrew Richardson',
+      }
+    ],
+  },
 }
 
 export default function RootLayout({
