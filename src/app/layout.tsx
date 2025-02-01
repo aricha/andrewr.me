6e83549542aaf7424a8a9a2e6bee1c0967189009
Andrew Richardson <andrewr114@gmail.com>
2025-01-31 21:32:03 -0800

    Fix favicon ico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,9 +15,7 @@ export const metadata: Metadata = {
       { url: '/icon.svg', sizes: 'any', type: 'image/svg+xml' },
       { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
       { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-    ],
-    apple: [
-      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
+      { url: '/favicon.ico', type: 'image/x-icon' },
     ],
   },
   openGraph: {
